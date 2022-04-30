393a1a95a48b1edf8ce3e7523e62692b6cc71bd0
user <user@DESKTOP-JHUM1BL>
2022-04-30 20:45:49 +0900

    Index/NewServletの修正

---
+++ WebContent/WEB-INF/views/messages/_form.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<label for="title">タイトル</label></br>
+<input type="text" name="title" vallue="${message.title}" />
+<br /><br />
+
+
+<label for="content">メッセージ</label><br />
+<input type="text" name="content" value="${message.content }" />
+<br /><br />
+
+
+<input type="hidden" name="_token" value="${_token}" />
+<button type="submit">投稿</button>
--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,8 +19,8 @@ import utils.DBUtil;
  */
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,16 +29,21 @@ public class IndexServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
-	    
-	    List<Message> messages = em.createNamedQuery("getAllMessages",Message.class).getResultList();
-	    response.getWriter().append(Integer.valueOf(messages.size()).toString());
-	    
-	    
-	    em.close();
-	    
-	}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages",Message.class).getResultList();
+
+
+        em.close();
+
+
+        request.setAttribute("messages", messages);
+
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
+    }
 
 }
