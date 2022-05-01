5e21bf247ff9d9e13f84462c6d2a1c96356218d7
user <user@DESKTOP-JHUM1BL>
2022-05-01 17:07:20 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -46,7 +46,11 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
 
-
+        // メッセージデータが存在しているときのみ
+        // メッセージIDをセッションスコープに登録
+        if(m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
         // メッセージIdをセッションスコープに登録
         request.getSession().setAttribute("message_id", m.getId());
 
